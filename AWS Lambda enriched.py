ceb3c0d7e40fefc55aae42c5b88075fa468f4a95
JosueMorfim <141301164+JosueMorfim@users.noreply.github.com>
2024-03-26 10:27:35 -0300

    Add files via upload

--- AWS Lambda enriched.py
+++ AWS Lambda enriched.py
@@ -12,7 +12,7 @@ def lambda_handler(event: dict, context: dict) -> bool:
 
   '''
   Diariamente é executado para compactar as diversas mensagensm, no formato
-  JSON, do dia anterior, armazenadas no bucket de dados cru, em um único 
+  JSON, do dia anterior, armazenadas no bucket de dados cru, em um único
   arquivo no formato PARQUET, armazenando-o no bucket de dados enriquecidos
   '''
 
@@ -25,8 +25,10 @@ def lambda_handler(event: dict, context: dict) -> bool:
 
   tzinfo = timezone(offset=timedelta(hours=-3))
   date = (datetime.now(tzinfo) - timedelta(days=1)).strftime('%Y-%m-%d')
-  #date = (datetime.now(tzinfo) - timedelta(days=0)).strftime('%Y-%m-%d')
   timestamp = datetime.now(tzinfo).strftime('%Y%m%d%H%M%S%f')
+
+  # código principal
+
   table = None
   client = boto3.client('s3')
 
@@ -35,7 +37,7 @@ def lambda_handler(event: dict, context: dict) -> bool:
       response = client.list_objects_v2(Bucket=RAW_BUCKET, Prefix=f'telegram/context_date={date}')
 
       for content in response['Contents']:
-      
+
         key = content['Key']
         client.download_file(RAW_BUCKET, key, f"/tmp/{key.split('/')[-1]}")
 
@@ -55,41 +57,91 @@ def lambda_handler(event: dict, context: dict) -> bool:
 
           table = iter_table
           iter_table = None
-          
+
       pq.write_table(table=table, where=f'/tmp/{timestamp}.parquet')
       client.upload_file(f"/tmp/{timestamp}.parquet", ENRICHED_BUCKET, f"telegram/context_date={date}/{timestamp}.parquet")
 
       return True
-  
+
   except Exception as exc:
       logging.error(msg=exc)
       return False
       
       
+      
 def parse_data(data: dict) -> dict:
 
-  date = datetime.now().strftime('%Y-%m-%d')
-  timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    date = datetime.now().strftime('%Y-%m-%d')
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-  parsed_data = dict()
+    parsed_data = dict()
 
-  for key, value in data.items():
+    for key, value in data.items():
 
-      if key == 'from':
-          for k, v in data[key].items():
-              if k in ['id', 'is_bot', 'first_name']:
-                parsed_data[f"{key if key == 'chat' else 'user'}_{k}"] = [v]
+        if key == 'from':
+            for k, v in data[key].items():
+                if k in ['id', 'is_bot', 'first_name']:
+                    parsed_data[f"{key if key == 'chat' else 'user'}_{k}"] = [v]
 
-      elif key == 'chat':
-          for k, v in data[key].items():
-              if k in ['id', 'type']:
-                parsed_data[f"{key if key == 'chat' else 'user'}_{k}"] = [v]
+        elif key == 'chat':
+            for k, v in data[key].items():
+                if k in ['id', 'type']:
+                    parsed_data[f"{key if key == 'chat' else 'user'}_{k}"] = [v]
 
-      elif key in ['message_id', 'date', 'text']:
-          parsed_data[key] = [value]
+        elif key in ['message_id', 'date', 'text', 'type']:
+            parsed_data[key] = [value]
+            parsed_data['type'] = ['text message']       
+        
+        elif key == 'video_note':
+                parsed_data['text'] = ["new recording video"]
+                parsed_data['type'] = ['video_note']          
+        
+        elif key == 'voice':
+            if 'mime_type' in value:
+                parsed_data['text'] = [f"voice message"]
+                parsed_data['type'] = [f"{value['mime_type']} "]    
+                
+        elif key == 'document':
+            if 'mime_type' in value:
+                parsed_data['text'] = [f"{value['file_name']} "]
+                parsed_data['type'] = [f"{value['mime_type']} "]    
+                
+        elif key == 'sticker':
+            if 'emoji' in value:
+                parsed_data['text'] = [f"{value['set_name']} - {value['emoji']}"]
+                parsed_data['type'] = ['sticker']                   
+             
+        elif key == 'poll':
+            if 'question' in value:
+                parsed_data['text'] = [f"{value['question']} - options : {value['options']}"]
+                parsed_data['type'] = ['poll']   
+            
+        elif key == 'photo':
+            if 'caption' in data:
+              parsed_data['text'] = [f"{data['caption']}"]
+              parsed_data['type'] = ["photo"]
+            else:
+              parsed_data['text'] = ["image file"]
+              parsed_data['type'] = ["photo"]
 
-  if not 'text' in parsed_data.keys():
-    parsed_data['text'] = [None]
-
-  return parsed_data
+        elif key == 'location':
+            if 'latitude' in value:
+                parsed_data['text'] = [f"latitude: {value['latitude']} - longitude: {value['longitude']}"]
+                parsed_data['type'] = ['location']  
       
+        elif key == 'new_chat_participant':
+            if 'first_name' in value:
+                parsed_data['text'] = [f"{value['first_name']} joined on group"]
+                parsed_data['type'] = ["new member"]      
+       
+              
+        
+    if 'text' not in parsed_data:
+        parsed_data['text'] = ['NA']
+
+    if 'type' not in parsed_data:
+        parsed_data['type'] = ['NA']
+        
+        
+
+    return parsed_data
